d5311754015aef2ed46f78d6a418e8f8c9aa3061
Christopher Enriquez <zeareau@Christophers-MacBook-Pro.local>
2025-11-05 17:08:36 -0500

    Add times to events

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -3,14 +3,14 @@ export const EVENTS = [
   {
     date: "Nov 13, 2025",
     title: "GBM 3 - Business Analytics - CGI",
-    where: "Duques 453",
+    where: "Duques 453 - 6:15pm",
     blurb: "Work with a CGI Business Analyst to analyze real business data.",
     tags: ["CGI", "Data Visualization", "Client-Work"],
   },
   {
     date: "Nov 7, 2025",
     title: "Georgetown Sunset Social",
-    where: "USC Start",
+    where: "USC Start - 4:30pm",
     blurb: "Join us on a walk to Georgetown for food, fun, and socializing!",
     tags: ["Networking", "Picnic", "Flicks"],
   },
