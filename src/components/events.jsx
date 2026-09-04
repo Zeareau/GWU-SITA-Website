2bb76dba2a213916f8e35b0b520e792802d9e720
asrvn <anieesh.saravanan@gmail.com>
2026-09-03 20:20:56 -0400

    Update events page

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,38 +1,10 @@
 // src/data/events.js
 export const EVENTS = [
   {
-    date: "Feb 5th | 6:00 PM - 8:00 PM",
-    title: "Industry Insights - AWS",
-    where: "Duques 155",
-    blurb: "Gain valuable industry insights from an AWS Cloud Consultant and previous ISTM alumni!",
-    tags: ["Amazon","AWS","Alumni","Consulting"],
-  },
-  {
-    date: "Feb 12th | 2:00 PM - 4:00 PM",
-    title: "Valentine's Day Fundraiser",
-    where: "U-Yard",
-    blurb: "Need a last minute Valentine's Day Gift? Come support us by and buy chocolate strawberries for your loved ones!",
-    tags: ["Valentine's Day","Fundraiser","Chocolate"],
-  },
-  {
-    date: "Mar 23rd | 6:00 PM - 8:00 PM",
-    title: "Industry Insights - NVIDIA",
-    where: "Duques 453",
-    blurb: "Gain valuable industry insights from an NVIDIA Reinforcement Learning Engineer!",
-    tags: ["NVIDIA","Alumni","AI/ML/RL"],
-  },
-  {
-    date: "Apr 10th | 6:00 PM - 8:00 PM",
-    title: "Tech Social",
-    where: "Duques 405",
-    blurb: "Reconnect with fellow alumni and make new connections over food and drinks!",
-    tags: ["Alumni","Networking","Social"],
-  },
-  {
-    date: "Apr 15th | 6:00 PM - 8:00 PM",
-    title: "Hackathon - Cloud Quest",
-    where: "Duques 150",
-    blurb: "Join us for a night of coding, collaboration, and innovation!",
-    tags: ["Hackathon","Coding","Innovation"],
-  },
+    date: "Sept 5 | 6:00 PM - 9:00 PM",
+    title: "Welcome Game Night & Social",
+    where: "USC 405",
+    blurb: "Meet the GW SITA E-Board and have some fun playing games!",
+    tags: ["Fun", "Games", "Networking", "Social"]
+  }
 ];
