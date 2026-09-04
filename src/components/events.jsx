9bfe86924c439fbb73f58c6365f9489f94fe3165
asrvn <anieesh.saravanan@gmail.com>
2026-09-04 19:43:05 -0400

    Update roster and archive past members

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -2,8 +2,8 @@
 export const EVENTS = [
   {
     date: "Sept 5 | 6:00 PM - 9:00 PM",
-    title: "Welcome Game Night & Social",
-    where: "USC 405",
+    title: "Welcome Game Night",
+    where: "University Student Center Room 405",
     blurb: "Meet the GW SITA E-Board and have some fun playing games!",
     tags: ["Fun", "Games", "Networking", "Social"]
   }
--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,79 +1,76 @@
 // src/data/team.js
-import chris from "../assets/chris_enriquez.jpeg";
-import delina from "../assets/delina_kahsay.jpeg";
-// import joven from "../assets/joven_torres.jpeg";
-import emily from "../assets/emily_rose.jpeg";
-import maryam from "../assets/maryam_khadim.jpeg";
-// import caitlyn from "../assets/caitlyn_nilo.jpeg";
-import careld from "../assets/careld_cruz.jpeg";
-import henry from "../assets/henry_lin.jpeg";
-import jaya from "../assets/jaya_seecharran.jpeg";
-import khadijah from "../assets/khadijah_nassim.jpeg";
-import anieesh from "../assets/anieesh_saravanan.jpeg";
-
+import chris from "../assets/members/chris_enriquez.jpeg";
+import delina from "../assets/members/delina_kahsay.jpeg";
+import careld from "../assets/members/careld_cruz.jpeg";
+import henry from "../assets/members/henry_lin.jpeg";
+import anieesh from "../assets/members/anieesh_saravanan.jpeg";
+import awa from "../assets/members/awa_badjie.jpg";
+import tosin from "../assets/members/tosin_olufowobi.jpg";
+import stacey from "../assets/members/stacey_chen.jpg";
+import unknown from "../assets/members/unknown.jpg";
 
 export const TEAM = [
   {
+    name: "Delina Kahsay",
+    role: "President",
+    linkedin: "https://www.linkedin.com/in/delinakahsay/",
+    img: delina,
+    github: "",
+  },
+  {
     name: "Chris Enriquez",
-    role: "Founder & President",
+    role: "Executive Vice President",
     linkedin: "https://www.linkedin.com/",
     img: chris,
     github: "https://github.com/Zeareau",
   },
   {
-    name: "Delina Kahsay",
-    role: "Executive Vice President",
-    linkedin: "https://www.linkedin.com/in/delinakahsay/",
-    img: delina,
-    github: "",
-  },
-    {
-    name: "Emily Rose",
-    role: "Vice President of Programming",
-    linkedin: "https://www.linkedin.com/in/emily-v-rose/",
-    img: emily,
-    github: "",
-  },
-    {
-    name: "Maryam Khadim",
-    role: "Vice President of Communications",
-    linkedin: "https://www.linkedin.com/in/maryam-khadimm/",
-    img: maryam,
-    github: "",
-  },
-    {
-    name: "Jaya Seecharran",
-    role: "Vice President of Social Media",
-    linkedin: "https://www.linkedin.com/in/jaya-seecharran/",
-    img: jaya,
-    github: "",
-  },
-      {
-    name: "Khadija Nassim",
-    role: "Vice President of Finance",
-    linkedin: "https://www.linkedin.com/in/khadija-nassim/",
-    img: khadijah,
-    github: "",
-  },
-    {
-    name: "Careld Cruz",
-    role: "Vice President of Graphic Design",
-    linkedin: "https://www.linkedin.com/in/careldcruza/",
-    img: careld,
-    github: "",
-  },
-      {
     name: "Henry Lin",
-    role: "Sophomore Representative",
+    role: "Vice President of Finance",
     linkedin: "https://www.linkedin.com/in/henry234lin/",
     img: henry,
     github: "",
   },
-        {
+  {
     name: "Anieesh Saravanan",
-    role: "Freshman Representative",
+    role: "Vice President of Operations & Competitions",
     linkedin: "https://www.linkedin.com/in/anieesh/",
     img: anieesh,
     github: "https://github.com/asrvn",
+  },
+  {
+    name: "Careld Cruz",
+    role: "Vice President of Social Media",
+    linkedin: "https://www.linkedin.com/in/careldcruza/",
+    img: careld,
+    github: "",
+  },
+  {
+    name: "Awa Badjie",
+    role: "Vice President of Programming",
+    linkedin: "https://www.linkedin.com/in/awabadjie/",
+    img: awa,
+    github: "",
+  },
+  {
+    name: "Tosin Olufowobi",
+    role: "Vice President of Communications",
+    linkedin: "https://www.linkedin.com/in/tosin-olufowobi-21b836274/",
+    img: tosin,
+    github: "",
+  },
+  {
+    name: "Stacey Chen",
+    role: "Vice President of Graphic Design",
+    linkedin: "https://www.linkedin.com/in/staceychen-/",
+    img: stacey,
+    github: "",
+  },
+  {
+    name: "You?",
+    role: "Freshman/Sophomore Representative",
+    linkedin: "",
+    img: unknown,
+    github: "",
   },
 ];
