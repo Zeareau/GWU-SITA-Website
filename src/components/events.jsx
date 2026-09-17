b1034245d3a77a053cd718ea1253acb0cf9dd9a3
asrvn <anieesh.saravanan@gmail.com>
2026-09-17 18:34:50 -0400

    Add event: Resume Workshop

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -1,6 +1,13 @@
 // src/data/events.js
 export const EVENTS = [
   {
+    date: "Sept 23 | 6:00 PM - 8:00 PM",
+    title: "Resume Workshop",
+    where: "Funger Hall Room 320",
+    blurb: "Level up your resume with tips and tricks for success!",
+    tags: ["Professional", "Advice", "Networking", "Career"]
+  },
+  {
     date: "Sept 5 | 6:00 PM - 9:00 PM",
     title: "Welcome Game Night",
     where: "University Student Center Room 405",
